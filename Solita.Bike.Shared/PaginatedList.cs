8378b6a2f128afadbc28cc5829fbdaf8d3358368
Joonas Valamäki <kafkalainen@gmail.com>
2023-05-31 02:04:55 +0300

    Added hard limit to 2000 in length.

--- Solita.Bike.Shared/PaginatedList.cs
+++ Solita.Bike.Shared/PaginatedList.cs
@@ -4,6 +4,7 @@ namespace Solita.Bike.Shared
 {
     public class PaginatedList<T> : List<T>
     {
+        private const int MaxPageSize = 2000;
         public int PageIndex { get; set; }
         
         public int PageSize { get; set; }
@@ -12,10 +13,10 @@ namespace Solita.Bike.Shared
         public bool HasPreviousPage => PageIndex > 1;
     
         public bool HasNextPage => PageIndex < TotalPages;
-        public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
+        public PaginatedList(IEnumerable<T> items, int count, int pageIndex, int pageSize)
         {
             PageIndex = pageIndex;
-            PageSize = pageSize;
+            PageSize = pageSize > MaxPageSize ? MaxPageSize : pageSize;
             TotalPages = (int)Math.Ceiling(count / (double)pageSize);
 
             AddRange(items);
