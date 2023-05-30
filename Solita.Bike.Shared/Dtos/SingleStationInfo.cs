43e8dd572becf4d1b0fa6f268c2ae773925b21e0
Joonas Valamäki <kafkalainen@gmail.com>
2023-05-30 23:39:17 +0300

    Added Dictionary instead of list of localizations to SingleStationInfo

--- Solita.Bike.Shared/Dtos/SingleStationInfo.cs
+++ Solita.Bike.Shared/Dtos/SingleStationInfo.cs
@@ -2,8 +2,8 @@ namespace Solita.Bike.Shared.Dtos;
 
 public class SingleStationInfo
 {
-    public List<Localization> Name { get; set; } = new ();
-    public List<Localization> Address { get; set; } = new ();
+    public Dictionary<Localization, string?> Name { get; set; } = new ();
+    public Dictionary<Localization, string?> Address { get; set; } = new ();
     public ulong StartJourneyTotal { get; set; }
     public ulong EndJourneyTotal { get; set; }
 }
