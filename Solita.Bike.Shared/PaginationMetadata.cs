3c98dd1c599400a570e2e6d2e36bde9f9ba181d4
Joonas Valamäki <kafkalainen@gmail.com>
2023-05-30 23:40:54 +0300

    Added simple booleans to PaginationMetaData.

--- Solita.Bike.Shared/PaginationMetadata.cs
+++ Solita.Bike.Shared/PaginationMetadata.cs
@@ -5,6 +5,6 @@ public class PaginationMetadata
     public int PageNumber { get; set; }
     public int PageSize { get; set; }
     public int TotalPages { get; set; }
-    public bool HasPreviousPage => PageNumber > 1;
-    public bool HasNextPage => PageNumber < TotalPages;
+    public bool HasPreviousPage { get; set; }
+    public bool HasNextPage { get; set; }
 }
