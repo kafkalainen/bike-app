789a3db2eea168b7c82d096289691945ab835839
Joonas Valamäki <kafkalainen@gmail.com>
2023-05-31 02:06:41 +0300

    Added hard limit to 2000.

--- Solita.Bike.Shared/PaginatedList.cs
+++ Solita.Bike.Shared/PaginatedList.cs
@@ -15,8 +15,8 @@ namespace Solita.Bike.Shared
         public bool HasNextPage => PageIndex < TotalPages;
         public PaginatedList(IEnumerable<T> items, int count, int pageIndex, int pageSize)
         {
-            PageIndex = pageIndex;
             PageSize = pageSize > MaxPageSize ? MaxPageSize : pageSize;
+            PageIndex = pageIndex;
             TotalPages = (int)Math.Ceiling(count / (double)pageSize);
 
             AddRange(items);
@@ -24,6 +24,7 @@ namespace Solita.Bike.Shared
 
         public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize)
         {
+            pageSize = pageSize > MaxPageSize ? MaxPageSize : pageSize;
             var count = await source.CountAsync();
             var items = await source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToListAsync();
             return new PaginatedList<T>(items, count, pageIndex, pageSize);
