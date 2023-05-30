e12c674b5f4bcb912c13d92a146952503d8fce06
Joonas Valamäki <kafkalainen@gmail.com>
2023-05-30 23:40:04 +0300

    Added StationInfo.

--- Solita.Bike.Shared/Dtos/StationInfo.cs
+++ Solita.Bike.Shared/Dtos/StationInfo.cs
@@ -3,9 +3,9 @@ namespace Solita.Bike.Shared.Dtos;
 public class StationInfo
 {
     public string? Id { get; set; }
-    public List<Localization>? Name{ get; set; }
-    public List<Localization>? Address { get; set; }
-    public List<Localization>? City { get; set; }
+    public Dictionary<Localization, string?>? Name{ get; set; }
+    public Dictionary<Localization, string?>? Address { get; set; }
+    public Dictionary<Localization, string?>? City { get; set; }
     public string? Operator { get; set; }
     public int Capacity { get; set; }
     public double X { get; set; }
